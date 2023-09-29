f49f711fb8aba2b36ff998e7b4bf94c98bd50e60
hungd <hungd@DESKTOP-5J17MJ3>
2023-09-29 07:39:20 +0700

    Add project files.

---
+++ khang/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace khang.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
